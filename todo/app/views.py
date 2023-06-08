d5e495523dee19cf04076eb1e24ff2eafb2f91ec
ilyas <ilyas_sosaev@mail.ru>
2023-06-08 17:42:29 +0500

    all works

--- todo/app/views.py
+++ todo/app/views.py
@@ -1,28 +1,30 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.utils.text import slugify
 
-from django.views import generic
 from . import forms
 from . import models
-
-from django.contrib import messages
 
 import datetime
 
 
-def index(request):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
+def if_logged_in(func):
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('users:login')
+        return func(request, *args, **kwargs)
+    return wrapper
 
+
+@if_logged_in
+def index(request):
     form = forms.AddTaskForm()
     
     return render(request, 'todo/main.html', { 'form' : form })
 
 
+@if_logged_in
 def add_task(request):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     if request.method == 'POST':
         form = forms.AddTaskForm(request.POST)
         if form.is_valid():
@@ -39,10 +41,8 @@ def add_task(request):
         return redirect('app:index')
 
 
+@if_logged_in
 def delete_task(request, pk):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     task = models.Task.objects.filter(user=request.user).get(pk=pk)
 
     if task:
@@ -51,10 +51,8 @@ def delete_task(request, pk):
     return redirect(request.META['HTTP_REFERER'])
 
 
+@if_logged_in
 def complete_task(request, pk):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     task = models.Task.objects.filter(user=request.user).get(pk=pk)
 
     if task:
@@ -64,26 +62,20 @@ def complete_task(request, pk):
     return redirect(request.META['HTTP_REFERER'])
 
 
+@if_logged_in
 def completed_tasks(request):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     return render(request, 'todo/completed_tasks.html')
 
 
+@if_logged_in
 def upcoming_tasks(request):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     form = forms.AddTaskForm()
 
     return render(request, 'todo/upcoming_tasks.html', { 'form' : form })
 
 
+@if_logged_in
 def add_folder(request):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     if request.method == 'POST':
         form = forms.AddFolderForm(request.POST)
         if form.is_valid():
@@ -98,10 +90,8 @@ def add_folder(request):
         return redirect('app:index')
 
 
+@if_logged_in
 def delete_folder(request, pk):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     folder = models.Folder.objects.filter(user=request.user).get(pk=pk)
     folder_tasks = folder.get_tasks()
 
@@ -114,10 +104,8 @@ def delete_folder(request, pk):
     return redirect('app:index')
 
 
+@if_logged_in
 def folder_tasks(request, folder_pk):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     folder = models.Folder.objects.filter(user=request.user).get(pk=folder_pk)
 
     if not folder:
@@ -126,10 +114,8 @@ def folder_tasks(request, folder_pk):
     return render(request, 'todo/folder_tasks.html', { 'folder' : folder })
 
 
+@if_logged_in
 def add_folder_task(request, folder_pk):
-    if not request.user.is_authenticated:
-        return redirect('users:login')
-    
     folder = models.Folder.objects.filter(user=request.user).get(pk=folder_pk)
 
     if not folder:
@@ -147,6 +133,6 @@ def add_folder_task(request, folder_pk):
             return redirect('app:folder_tasks', folder_pk=folder.id)
         else:
             messages.success(request, f'{form.errors}')
-            return render(request, 'todo/folder_tasks.html')
+            return redirect('app:folder_tasks', folder_pk=folder.id)
     else:
         return redirect('app:index')
