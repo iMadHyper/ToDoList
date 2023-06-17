5eaf61b2d1e9d73e98fe85d65a91bb370662996a
ilyas <ilyas_sosaev@mail.ru>
2023-06-17 19:38:37 +0500

    fixed views and templates

--- todo/app/views.py
+++ todo/app/views.py
@@ -2,15 +2,22 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.utils.text import slugify
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from . import forms
 from . import models
 
 import datetime
 
-import re
+
+# Renders errors passsed in messages
+ERRORS_TEMPLATE = 'todo/errors.html'
 
 
 def if_logged_in(func):
+    '''
+    Redirects the user to login page if he's not logged in
+    '''
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect('users:login')
@@ -31,7 +38,7 @@ def index(request):
             if not cd['date']:
                 cd['date'] = datetime.date.today()
             elif cd['date'] < datetime.date.today():
-                messages.success(request, 'Date can\'t be past!')
+                messages.success(request, 'Date and time can\'t be past!')
                 return render(request, template_name)
 
             if cd['time']:
@@ -52,70 +59,53 @@ def index(request):
         return render(request, template_name, { 'form' : form })
 
 
-def respect_redirect_after_adding_task(request):
-    '''
-    Redirect to template from request if possible
-    '''
-    url = '/'.join(request.META['HTTP_REFERER'].split('//')[1].split('/')[1:])
-    print(url)
-    if url == 'app/upcoming/':
-        return redirect('app:upcoming_tasks')
-    else:
-        return redirect('app:index')
-    
-
-def respect_render_after_adding_task(request, msg=None):
-    '''
-    Render the template from request if possible
-    '''
-    url = '/'.join(request.META['HTTP_REFERER'].split('//')[1].split('/')[1:])
-    if url == 'app/upcoming/':
-        return render(request, 'todo/upcoming_tasks.html', { 'msg' : msg })
-    else:
-        return render(request, 'todo/main.html', { 'msg' : msg })
-
-
 @if_logged_in
-def add_task(request):
+def upcoming_tasks(request):
+    template_name = 'todo/upcoming_tasks.html'
     if request.method == 'POST':
         form = forms.AddTaskForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             cd['user'] = request.user
-            print(cd['time'])
 
             if not cd['date']:
                 cd['date'] = datetime.date.today()
             elif cd['date'] < datetime.date.today():
-                msg = 'Date can\'t be past!'
-                return respect_render_after_adding_task(request, msg=msg)
-            
+                messages.success(request, 'Date and time can\'t be past!')
+                return render(request, template_name)
+
             if cd['time']:
                 now = datetime.datetime.now()
                 t1 = datetime.timedelta(hours=cd['time'].hour, minutes=cd['time'].minute, seconds=cd['time'].second)
                 t2 = datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second)
                 if t1 < t2:
-                    msg = 'Date and time can\'t be past!'
-                    return respect_render_after_adding_task(request, msg=msg)
+                    messages.success(request, 'Date and time can\'t be past!')
+                    return render(request, template_name)
             
             models.Task.objects.create(**cd)
-            return respect_redirect_after_adding_task(request)
-        
+            return render(request, template_name)
         else:
-            return respect_render_after_adding_task(request, msg=form.errors)
+            messages.success(request, f'{form.errors}')
+            return render(request, template_name)
     else:
-        try:
-            return redirect(request.META['HTTP_REFERER'])
-        except:
-            return redirect('app:index')
+        form = forms.AddTaskForm()
+        return render(request, template_name, { 'form' : form })
+
+
+@if_logged_in
+def completed_tasks(request):
+    return render(request, 'todo/completed_tasks.html')
 
 
 @if_logged_in
 def delete_task(request, pk):
-    task = models.Task.objects.filter(user=request.user).get(pk=pk)
+    try:
+        task = models.Task.objects.filter(user=request.user).filter(is_completed=False).get(pk=pk)
+    except:
+        messages.success(request, 'The task matching query does not exist!')
+        return render(request, ERRORS_TEMPLATE)
 
-    if task:
-        task.delete()
+    task.delete()
 
     try:
         return redirect(request.META['HTTP_REFERER'])
@@ -125,55 +115,17 @@ def delete_task(request, pk):
 
 @if_logged_in
 def complete_task(request, pk):
-    task = models.Task.objects.filter(user=request.user).get(pk=pk)
+    try:
+        task = models.Task.objects.filter(user=request.user).filter(is_completed=False).get(pk=pk)
+    except ObjectDoesNotExist:
+        messages.success(request, 'The task matching query does not exist!')
+        return render(request, ERRORS_TEMPLATE)
 
-    if task:
-        task.is_completed = True
-        task.save()
+    task.is_completed = True
+    task.save()
 
     try:
         return redirect(request.META['HTTP_REFERER'])
-    except:
-        return redirect('app:index')
-
-
-@if_logged_in
-def completed_tasks(request):
-    return render(request, 'todo/completed_tasks.html')
-
-
-@if_logged_in
-def upcoming_tasks(request):
-    form = forms.AddTaskForm()
-
-    return render(request, 'todo/upcoming_tasks.html', { 'form' : form })
-
-
-redirects = {
-    'app/today/' : 'app:index',
-    'app/upcoming/' : 'app:upcoming_tasks',
-    'app/completed/' : 'app:completed_tasks',
-}
-
-renders = {
-
-}
-
-def respect_redirect(request):
-    '''
-    Redirect to template from request if possible
-    '''
-    url = '/'.join(request.META['HTTP_REFERER'].split('//')[1].split('/')[1:]).strip()
-    print(url)
-    try:
-        if re.match(r'^app/(\d+)/$', url):
-            print('true')
-            print(url.split('/')[1])
-            try:
-                return redirect('app:folder_tasks', folder_pk=int(url.split('/')[1]))
-            except:
-                return redirect('app:index')
-        return redirect(redirects[url])
     except:
         return redirect('app:index')
 
@@ -188,54 +140,64 @@ def add_folder(request):
             folder = models.Folder.objects.create(**cd)
             return redirect('app:folder_tasks', folder_pk=folder.id)
         else:
-            return respect_redirect(request)
+            messages.success(request, 'The form is not valid!')
+            return render(request, ERRORS_TEMPLATE)
     else:
-        return respect_redirect(request)
+        return redirect('app:index')
 
 
 @if_logged_in
 def delete_folder(request, pk):
-    folder = models.Folder.objects.filter(user=request.user).get(pk=pk)
-    folder_tasks = folder.get_tasks()
+    try:
+        folder = models.Folder.objects.filter(user=request.user).get(pk=pk)
+    except:
+        messages.success(request, 'The folder matching query does not exist!')
+        return render(request, ERRORS_TEMPLATE)
 
+    folder_tasks = folder.get_tasks()
     if folder_tasks:
         folder_tasks.delete()
-
-    if folder:
-        folder.delete()
+    folder.delete()
 
     return redirect('app:index')
 
 
 @if_logged_in
 def folder_tasks(request, folder_pk):
-    folder = models.Folder.objects.filter(user=request.user).get(pk=folder_pk)
+    template_name = 'todo/folder_tasks.html'
 
-    if not folder:
-        return redirect(request.META['HTTP_REFERER'])
-    
-    return render(request, 'todo/folder_tasks.html', { 'folder' : folder })
+    try:
+        folder = models.Folder.objects.filter(user=request.user).get(pk=folder_pk)
+    except:
+        messages.success(request, 'The folder matching query does not exist!')
+        return render(request, ERRORS_TEMPLATE)
 
-
-@if_logged_in
-def add_folder_task(request, folder_pk):
-    folder = models.Folder.objects.filter(user=request.user).get(pk=folder_pk)
-
-    if not folder:
-        return redirect(request.META['HTTP_REFERER'])
-    
     if request.method == 'POST':
         form = forms.AddTaskForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             cd['user'] = request.user
             cd['folder'] = folder
+
             if not cd['date']:
                 cd['date'] = datetime.date.today()
+            elif cd['date'] < datetime.date.today():
+                messages.success(request, 'Date and time can\'t be past!')
+                return render(request, template_name, { 'folder' : folder })
+            
+            if cd['time']:
+                now = datetime.datetime.now()
+                t1 = datetime.timedelta(hours=cd['time'].hour, minutes=cd['time'].minute, seconds=cd['time'].second)
+                t2 = datetime.timedelta(hours=now.hour, minutes=now.minute, seconds=now.second)
+                if t1 < t2:
+                    messages.success(request, 'Date and time can\'t be past!')
+                    return render(request, template_name, { 'folder' : folder })
+            
             models.Task.objects.create(**cd)
-            return redirect('app:folder_tasks', folder_pk=folder.id)
+            return render(request, template_name, { 'folder' : folder })
         else:
             messages.success(request, f'{form.errors}')
-            return redirect('app:folder_tasks', folder_pk=folder.id)
+            return render(request, template_name, { 'folder' : folder })
     else:
-        return redirect('app:index')
+        form = forms.AddTaskForm()
+        return render(request, template_name, { 'folder' : folder, 'form' : form })
