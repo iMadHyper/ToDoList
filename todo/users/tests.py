68672c7459ae4d048ce34f79c9b04e1b963b10a8
ilyas <ilyas_sosaev@mail.ru>
2023-06-20 19:27:56 +0500

    users-app test

--- todo/users/tests.py
+++ todo/users/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django import urls
+
+
+@pytest.mark.parametrize('view_name', ['users:login', 'users:signup'])
+def test_auth_views_are_available(client, view_name):
+    '''Auth pages are available to a non authenticated user'''
+    url = urls.reverse(view_name)
+    resp = client.get(url)
+    assert resp.status_code == 200
